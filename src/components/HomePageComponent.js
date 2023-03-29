7ae0e3f9ced9f90d7508c9769fd226a1a6073071
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-03-29 12:39:08 +0200

    Changes in HomePageComponent

--- src/components/HomePageComponent.js
+++ src/components/HomePageComponent.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 
 export default class HomePage extends Component {
-  render() {}
+  render() {
+    return (
+      <form>
+        <h3>Dobrodošli</h3>
+
+        <p className="home-page text-right"></p>
+      </form>
+    );
+  }
 }
