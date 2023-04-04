7a23d456a274322fc0e4e3f6095ae487d35a0a44
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-04-04 13:49:19 +0200

    changes in HomePage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Login from "./screens/LoginComponent";
-import SignUp from "./screens/SignUpComponent";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
 import ForgotPassword from "./screens/ForgotPassword";
-import HomePage from "./screens/HomePageComponent";
+import HomePage from "./screens/HomePage";
 import "./index.css";
 
 function App() {
@@ -13,33 +13,29 @@ function App() {
     <Router>
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/sign-in"}>
-              <img
-                src="/assets/images/logo.png"
-                alt="MyTaskBuddy Logo"
-                width="30"
-                height="30"
-                className="d-inline-block align-text-top me-2"
-              />
-              MyTaskBuddy
-            </Link>
+          <Link className="navbar-brand" to={"/sign-in"}>
+            <img
+              src="/assets/images/logo.png"
+              alt="MyTaskBuddy Logo"
+              width="30"
+              height="30"
+              className="d-inline-block align-text-top me-2"
+            />
+            MyTaskBuddy
+          </Link>
 
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-in"}>
-                    Prijavi se
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-up"}>
-                    Registruj se
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to={"/sign-in"}>
+                Prijavi se
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/sign-up"}>
+                Registruj se
+              </Link>
+            </li>
+          </ul>
         </nav>
 
         <Routes>
