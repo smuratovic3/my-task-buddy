3380c6ad7587ce5c59ffbd11b55cd3bf3d647ce9
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-04-04 13:56:52 +0200

    update in ForgotPassword screen

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
+import ForgotPasswordCSS from "../css/forgotpassword.module.css";
 
 export default class ForgotPassword extends Component {
   render() {
     return (
       <form>
-        <h3>Zaboravili ste lozinku</h3>
+        <div className={ForgotPasswordCSS.wrapper}>
+          <div className={ForgotPasswordCSS.inner}>
+            <h3>Zaboravili ste lozinku</h3>
 
-        <div className="mb-3">
-          <label>E-MAIL ADRESA</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Unesite email"
-          />
+            <div className="mb-3">
+              <label>E-MAIL ADRESA</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Unesite email"
+              />
+            </div>
+
+            <div className="d-grid">
+              <button type="submit" className="btn btn-success">
+                Pošalji
+              </button>
+            </div>
+
+            <p className="forgot-password text-right">
+              Na E-mailu ćete dobiti novu lozinku!
+            </p>
+          </div>
         </div>
-
-        <div className="d-grid">
-          <button type="submit" className="btn btn-success">
-            Pošalji
-          </button>
-        </div>
-
-        <p className="forgot-password text-right">
-          Na E-mailu ćete dobiti novu lozinku!
-        </p>
       </form>
     );
   }
